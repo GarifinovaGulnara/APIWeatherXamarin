bb64e4dbc8c94d705cc107a67c905dd1ad4ebc80
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-05-13 09:01:46 +0300

    Добавьте файлы проекта.

---
+++ APIWeatherXamarin/APIWeatherXamarin/Model/WeatherModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIWeatherXamarin.Model
+{
+    public class WeatherModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Temp { get; set; }
+        public int GTemp { get; set; }
+        public string Wind { get; set; }
+        public double WindDirection { get; set; }
+        public int Humidity { get; set; }
+        public int Pressure { get; set; }
+    }
+}
