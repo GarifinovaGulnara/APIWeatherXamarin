b318670b5739f95620841ae159e6c835df3a2d5d
Гульнара Гарифинова <gulnaragarifinova@yandex.ru>
2022-05-21 09:49:55 +0300

    location of the city on the map

--- APIWeatherXamarin/APIWeatherXamarin.Android/MainActivity.cs
+++ APIWeatherXamarin/APIWeatherXamarin.Android/MainActivity.cs
@@ -4,12 +4,19 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android;
 
 namespace APIWeatherXamarin.Droid
 {
     [Activity(Label = "APIWeatherXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const int RequestLoactionID = 0;
+        readonly string[] locationPermission =
+        {
+           Manifest.Permission.AccessCoarseLocation,
+           Manifest.Permission.AccessFineLocation
+        };
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,6 +30,18 @@ namespace APIWeatherXamarin.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                if (CheckSelfPermission(Manifest.Permission.AccessCoarseLocation) != Permission.Granted)
+                {
+                    RequestPermissions(locationPermission, RequestLoactionID);
+                }
+            }
         }
     }
 }
--- APIWeatherXamarin/APIWeatherXamarin/Views/WeatherPage.xaml.cs
+++ APIWeatherXamarin/APIWeatherXamarin/Views/WeatherPage.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace APIWeatherXamarin.Views
@@ -7,9 +11,12 @@ namespace APIWeatherXamarin.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WeatherPage : ContentPage
     {
+        CancellationTokenSource cts;
+        Pin pin = new Pin();
         public WeatherPage()
         {
             InitializeComponent();
+            var res = GetcurrLocation();
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -22,6 +29,25 @@ namespace APIWeatherXamarin.Views
             WindLbl.Text = "направление ветра" + " " + a.wind.gust.ToString();
             HumidityLbl.Text = "влажность" + " " + a.main.humidity.ToString();
             PressureLbl.Text = "давление" + " " + a.main.pressure.ToString();
+            Position position = new Position(a.coord.lat, a.coord.lon);
+
+            Map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(a.coord.lat, a.coord.lon), Distance.FromMiles(1)));
+            pin.Position = new Position(a.coord.lat, a.coord.lon);
+        }
+
+        async Task<Location> GetcurrLocation()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+            cts = new CancellationTokenSource();
+            var location = await Geolocation.GetLocationAsync(request, cts.Token);
+            if (location != null)
+            {
+                Map.MoveToRegion (
+                    MapSpan.FromCenterAndRadius(new Position (location.Latitude,
+                        location.Longitude), Distance.FromKilometers(1))
+                        );
+            }
+            return location;
         }
     }
 }
